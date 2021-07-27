65364b7555f3a9c6efae106d1b3b1ea6ca497f3e
Marcos Torralbas <matnldb@gmail.com>
2021-07-27 11:17:39 -0400

    probando hooks-docs

---
+++ docs/static/js/main.117ce0a0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcounter-app"]=this["webpackJsonpcounter-app"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);c(1);var n=c(3),r=c.n(n),o=c(0),s=function(){return Object(o.jsxs)("div",{children:[Object(o.jsx)("h1",{children:"Multiple Custom Hooks"}),Object(o.jsx)("hr",{})]})},u=(c(9),document.querySelector("#root"));r.a.render(Object(o.jsx)(s,{}),u)},9:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.117ce0a0.chunk.js.map
